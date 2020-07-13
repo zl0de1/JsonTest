71171372fc3ed5932649f2254665294abcbb869c
Konstantin <morderfaes@gmail.com>
2020-07-13 17:12:58 +0300

    init

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace JsonTest
 {
@@ -10,7 +10,13 @@ namespace JsonTest
     {
         static void Main(string[] args)
         {
-
+            string input = File.ReadAllText("tx.txt");
+            Response res = JsonConvert.DeserializeObject<Response>(input);
+            Console.WriteLine(res.id + res.last_name);
+            string json = File.ReadAllText("tx2.txt");
+            Response res2 = JsonConvert.DeserializeObject<Response>(json);
+            Console.WriteLine(res2.id + " " + res2.last_name);
+            Console.ReadKey();
         }
     }
 }
---
+++ User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonTest
+{
+    public class Response
+    {
+        public int id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public bool is_closed { get; set; }
+        public bool can_access_closed { get; set; }
+
+    }
+}
