01793158f7d538620dafa60deffa3a016becb703
Konstantin <morderfaes@gmail.com>
2020-07-13 18:04:01 +0300

    work

--- Program.cs
+++ Program.cs
@@ -11,8 +11,9 @@ namespace JsonTest
         static void Main(string[] args)
         {
             string input = File.ReadAllText("tx.txt");
-            Response res = JsonConvert.DeserializeObject<Response>(input);
-            Console.WriteLine(res.id + res.last_name);
+            User res = JsonConvert.DeserializeObject<User>(input);
+            Console.WriteLine(res.response[0].id + res.response[0].last_name);
+            
             string json = File.ReadAllText("tx2.txt");
             Response res2 = JsonConvert.DeserializeObject<Response>(json);
             Console.WriteLine(res2.id + " " + res2.last_name);
--- User.cs
+++ User.cs
@@ -12,7 +12,11 @@ namespace JsonTest
         public string first_name { get; set; }
         public string last_name { get; set; }
         public bool is_closed { get; set; }
-        public bool can_access_closed { get; set; }
+        public bool can_access_closed { get; set; }  
+    }
 
+    public class User
+    {
+        public List<Response> response { get; set; }
     }
 }
